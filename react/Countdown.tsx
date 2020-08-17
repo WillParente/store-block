59a43e3c296df0443f9d15b6de87bda5f0537783
Willian Parente <willianparente@hotmail.com>
2020-08-16 21:52:59 -0300

    Complete course

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TimeSplit } from './typings/global';
+import { tick } from './utils/time';
+import { useCssHandles } from 'vtex.css-handles';
+import { useQuery } from 'react-apollo';
+import useProduct from 'vtex.product-context/useProduct';
+import productReleaseDate from './queries/productReleaseDate.graphql';
 
-interface CountdownProps { }
-
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+interface CountdownProps {
+  targetDate: string;
 }
+
+const DEFAULT_TARGET_DATE = new Date('2020-06-25').toISOString();
+const CSS_HANDLES = ['countdown'];
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = () => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00',
+  });
+
+  const handles = useCssHandles(CSS_HANDLES);
+
+  const productContext = useProduct()
+  const product = productContext?.product
+  const linkText = product?.linkText
+  const { data, loading, error } = useQuery(productReleaseDate, {
+    variables: {
+      slug: linkText,
+    },
+    ssr: false,
+  });
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime)
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div>
+        <span>Error!</span>
+      </div>
+    );
+  }
+  
+  return (
+    <div className={`${handles.countdown} db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    </div>
+  );
+};
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
-}
+  properties: {
+    targetDate: {
+      title: 'Final date',
+      description: 'Final date used in the countdown',
+      type: 'string',
+      default: null,
+    },
+  },
+};
 
-export default Countdown
+export default Countdown;
